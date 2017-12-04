1e502534027af8da9688ae9360124be9351a1df2
unknown <Mission@DESKTOP-9PR5H46>
2017-12-04 14:27:20 -0500

    Insert data to department

---
+++ Data/HumanResources.Department_Data.sql
@@ -0,0 +1,18 @@
+SET IDENTITY_INSERT [HumanResources].[Department] ON
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate]) VALUES (1, N'Engineering', N'Research and Development', '1998-06-01 00:00:00.000')
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate]) VALUES (2, N'Tool Design', N'Research and Development', '1998-06-01 00:00:00.000')
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate]) VALUES (3, N'Sales', N'Sales and Marketing', '1998-06-01 00:00:00.000')
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate]) VALUES (4, N'Marketing', N'Sales and Marketing', '1998-06-01 00:00:00.000')
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate]) VALUES (5, N'Purchasing', N'Inventory Management', '1998-06-01 00:00:00.000')
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate]) VALUES (6, N'Research and Development', N'Research and Development', '1998-06-01 00:00:00.000')
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate]) VALUES (7, N'Production', N'Manufacturing', '1998-06-01 00:00:00.000')
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate]) VALUES (8, N'Production Control', N'Manufacturing', '1998-06-01 00:00:00.000')
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate]) VALUES (9, N'Human Resources', N'Executive General and Administration', '1998-06-01 00:00:00.000')
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate]) VALUES (10, N'Finance', N'Executive General and Administration', '1998-06-01 00:00:00.000')
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate]) VALUES (11, N'Information Services', N'Executive General and Administration', '1998-06-01 00:00:00.000')
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate]) VALUES (12, N'Document Control', N'Quality Assurance', '1998-06-01 00:00:00.000')
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate]) VALUES (13, N'Quality Assurance', N'Quality Assurance', '1998-06-01 00:00:00.000')
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate]) VALUES (14, N'Facilities and Maintenance', N'Executive General and Administration', '1998-06-01 00:00:00.000')
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate]) VALUES (15, N'Shipping and Receiving', N'Inventory Management', '1998-06-01 00:00:00.000')
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate]) VALUES (16, N'Executive', N'Executive General and Administration', '1998-06-01 00:00:00.000')
+SET IDENTITY_INSERT [HumanResources].[Department] OFF
